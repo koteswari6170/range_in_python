2b0bcabd4c0cb0acd95b3cc889a963ef500c54bf
koteswari6170 <102402089+koteswari6170@users.noreply.github.com>
2022-04-12 12:35:41 +0530

    Add files via upload

---
+++ range.py
@@ -0,0 +1,39 @@
+# a=1,2,3,4,5
+# print(a)#(1,2,3,4,5)
+# print(*a)#1 2 3 4 5 
+
+# a=10,20,40
+# for j in a:
+    # print(j,end=" ") #10 20 40
+
+# for i in range(5):
+    # print(i)
+
+# n=int(input("Enter the value of n:"))
+# for i in range(1,n+1):
+    # print(i,end="\n")
+
+# for i in range(1,30,2):
+    # print(i,end=" ")#1 3 5 7 9 11 13 15 17 19 21 23 25 27 29
+
+# for i in range(2,30,2):
+    #  print(i,end=" ")#2 4 6 8 10 12 14 16 18 20 22 24 26 28
+
+# for i in range(10,0,-1):
+    # print(i)
+
+# for i in range(10,-1,-1):
+    # print(i)
+
+# n=[1,4.5,'kaveri']
+# for i in n[::-1]:
+    # print(i)
+
+# v='programming'
+# for e in v[::2]:
+    # print(e,end=" ")#p o r m i g
+
+
+
+
+
